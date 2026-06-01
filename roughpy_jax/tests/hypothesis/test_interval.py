bbf6b5fb00c605ca2b6b61773b1143cf42b16b28
Sam Morley <admin@inakleinbottle.com>
2026-06-01 14:30:09 +0100

    hypothesis tests take a long time to set up

--- roughpy_jax/tests/hypothesis/test_interval.py
+++ roughpy_jax/tests/hypothesis/test_interval.py
@@ -1,8 +1,10 @@
 from hypothesis import given
+import pytest
 
 from roughpy_jax.intervals import IntervalType
 from roughpy_jax.strategies import intervals
 
+@pytest.mark.extra
 @given(interval=intervals(IntervalType.ClOpen))
 def test_interval(interval):
     assert interval.interval_type == IntervalType.ClOpen
